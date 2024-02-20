63c4ee716b38a2f55c103a134645d6abdb3a146b
HarishaGB <harshagb555@gmail.com>
2024-02-20 22:39:43 +0530

    first commit

--- src/App.js
+++ src/App.js
@@ -1,23 +1,31 @@
-import logo from './logo.svg';
-import './App.css';
+import React,{useState} from "react";
+import "./App.css";
 
-function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+//pages 1 imports
+
+import About from "./Pages/About";
+import Contact from "./Pages/Contact";
+import Home from "./Pages/Home";
+import Blog from "./Pages/Blog";
+import {Routes, Route} from "react-router-dom";
+import Navbar from "./Pages/Navbar";
+
+
+const App = () =>{
+
+  const [count, setCount] = useState(0);
+
+  return(
+    
+    <div>
+      <Navbar/>
+      <Routes>
+        <Route path="/home" element={<Home name="Harisha G B"/>} />
+        <Route path="/blog" element={<Blog count={count} setCount={setCount}/>} />
+        <Route path="/about" element={<About />} />
+        <Route path="/contact" element={<Contact count={count} />} />
+      </Routes>
+      
     </div>
   );
 }
---
+++ src/Pages/Contact.js
@@ -0,0 +1,24 @@
+import React from "react";
+import { useNavigate } from "react-router-dom";
+
+
+const Contact = ({count}) =>{
+
+    let navigate = useNavigate()
+         
+
+
+    return(
+        <div>
+            <h1>Value of count - {count}</h1>
+            <p>This is Contact page</p>
+            <button onClick={() => navigate("/blog")}> Go to Blog 1</button>
+            {/* <button onClick={() => navigate("blog")}> Go to Blog 2</button> */}
+
+            <button onClick={() =>navigate(-1)}>Back</button>
+            <button onClick={() =>navigate(-1)}>Next</button>
+        </div>
+    );
+}
+
+export default Contact;
